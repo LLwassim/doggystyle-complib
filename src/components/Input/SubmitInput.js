4edf8d9ee70d275df7d27932f4603c9bcca93d30
blake <blakeeh723@gmail.com>
2019-11-20 12:21:02 -0500

    trying functional component

--- src/components/Input/SubmitInput.js
+++ src/components/Input/SubmitInput.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './SubmitInput.css';
 
-class SubmitInput extends React.Component {
-  render() {
-    return <input type="submit" value="Submit" />;
-  }
+const SubmitInput = () => (
+   <input type="submit" value="Submit" />
 };
 
 export default SubmitInput;
